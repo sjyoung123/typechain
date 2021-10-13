26298188a777277350d6731a3eab8d14f6a32263
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-13 21:14:53 +0900

    Set Typescript

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hello");
+console.log("great");
+var st = function () { return console.log("hihihi"); };
+st();
+console.log("hi");
+console.log("fine");
+console.log("h");
+//# sourceMappingURL=index.js.map
