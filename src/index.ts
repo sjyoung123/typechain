b68ae0a0a1377e30bf28a3c0540ff5e89baef381
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-14 21:32:29 +0900

    Updated calculateBlockHash

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -21,5 +32,10 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "12313", "", "hello", 123);
 
-let blockchain: [Block] = [genesisBlock];
-console.log(blockchain);
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlockchain = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
